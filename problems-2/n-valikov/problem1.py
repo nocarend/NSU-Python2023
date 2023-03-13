c90202c636773d7b79243e64414d274056d7306c
nocarend <58514394+nocarend@users.noreply.github.com>
2023-03-13 07:43:03 +0300

    add problem1

---
+++ problems-2/n-valikov/problem1.py
@@ -0,0 +1,9 @@
+def solve(n: int) -> [(int, int, int)]:
+	n += 1
+	return [(x, y, z) for x in range(1, n) for y in range(x, n) for z in
+	        range(y, n) if x ** 2 + y ** 2 == z ** 2]
+
+
+if __name__ == '__main__':
+	print("Please, print a number.")
+	print(solve(int(input())))
