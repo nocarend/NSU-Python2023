33c8cf3f30419ba45d310e1526aad96120bbf979
nocarend <58514394+nocarend@users.noreply.github.com>
2023-03-13 11:41:08 +0300

    add problem2

---
+++ problems-3/n-valikov/problem2.py
@@ -0,0 +1,305 @@
+'''
+https://en.wikipedia.org/wiki/Family_tree_of_Russian_monarchs#Romanov_dynasty
+'''
+
+
+class Mikhail_I:
+	pass
+
+
+class Maria_Dolgorukova:
+	pass
+
+
+class Evdokia_Streshneva:
+	pass
+
+
+class Irina(Mikhail_I, Evdokia_Streshneva):
+	pass
+
+
+class Pelagiya(Mikhail_I, Evdokia_Streshneva):
+	pass
+
+
+class Anna(Mikhail_I, Evdokia_Streshneva):
+	pass
+
+
+class Marfa(Mikhail_I, Evdokia_Streshneva):
+	pass
+
+
+class Ivan(Mikhail_I, Evdokia_Streshneva):
+	pass
+
+
+class Tatyana(Mikhail_I, Evdokia_Streshneva):
+	pass
+
+
+class Sofiya(Mikhail_I, Evdokia_Streshneva):
+	pass
+
+
+class Vasily(Mikhail_I, Evdokia_Streshneva):
+	pass
+
+
+class Yevdokiya(Mikhail_I, Evdokia_Streshneva):
+	pass
+
+
+class Alexis(Mikhail_I, Evdokia_Streshneva):
+	pass
+
+
+class Maria_Miloslavskaya:
+	pass
+
+
+class Natalya_Naryshkina:
+	pass
+
+
+class Dmitry(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Agafya_Grushetskaya():
+	pass
+
+
+class Feodor_III(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Marfa_Apraksina:
+	pass
+
+
+class Simeon(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Catherine(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Feodosia(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Maria(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Praskovia_Saltykova:
+	pass
+
+
+class Ivan_V(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Marfa_(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Alexei(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Sofia(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Yevdokia_(Alexis, Maria_Miloslavskaya):
+	pass
+
+
+class Maria_(Ivan_V, Praskovia_Saltykova):
+	pass
+
+
+class Ivan_Dmitriev_Mamonov:
+	pass
+
+
+class Praskovya(Ivan_V, Praskovia_Saltykova):
+	pass
+
+
+class Feodosia_(Ivan_V, Praskovia_Saltykova):
+	pass
+
+
+class Karl_Leopold:
+	pass
+
+
+class Catherine_(Ivan_V, Praskovia_Saltykova):
+	pass
+
+
+class Frederick_William:
+	pass
+
+
+class Anna_(Ivan_V, Praskovia_Saltykova):
+	pass
+
+
+class Natalya(Alexis, Natalya_Naryshkina):
+	pass
+
+
+class Fyodora(Alexis, Natalya_Naryshkina):
+	pass
+
+
+class Eudoxia_Lopukhina:
+	pass
+
+
+class Peter_I(Alexis, Natalya_Naryshkina):
+	pass
+
+
+class Catherine_I:
+	pass
+
+
+class Charlotte_Christine:
+	pass
+
+
+class Alexei_(Peter_I, Catherine_I):
+	pass
+
+
+class Alexander(Peter_I, Catherine_I):
+	pass
+
+
+class Pavel(Peter_I, Catherine_I):
+	pass
+
+
+class Anna_Leopoldovna(Karl_Leopold, Catherine):
+	pass
+
+
+class Anthony_Ulrich:
+	pass
+
+
+class Peter_II(Charlotte_Christine, Alexei_):
+	pass
+
+
+class Peter(Peter_I, Catherine_I):
+	pass
+
+
+class Paul(Peter_I, Catherine_I):
+	pass
+
+
+class Catherine__(Peter_I, Catherine_I):
+	pass
+
+
+class Elizabeth(Peter_I, Catherine_I):
+	pass
+
+
+class Ivan_VI(Anna_Leopoldovna, Anthony_Ulrich):
+	pass
+
+
+class Charles_Frederick:
+	pass
+
+
+class Anna__(Peter_I, Catherine_I):
+	pass
+
+
+class Maria__(Peter_I, Catherine_I):
+	pass
+
+
+class Peter_(Peter_I, Catherine_I):
+	pass
+
+
+class Pavel_(Peter_I, Catherine_I):
+	pass
+
+
+class Natalia(Peter_I, Catherine_I):
+	pass
+
+
+class Peter_III(Charles_Frederick, Anna__):
+	pass
+
+
+class Catherine_II:
+	pass
+
+
+class Natalia_Alexeievna:
+	pass
+
+
+class Paul_I(Peter_III, Catherine_II):
+	pass
+
+
+class Maria_Feodorovna:
+	pass
+
+
+class Alexei_Bobrinsky(Peter_III, Catherine_II):
+	pass
+
+
+class Anna__(Peter_III, Catherine_II):
+	pass
+
+
+class Juliane:
+	pass
+
+
+class Konstantin(Paul_I, Maria_Feodorovna):
+	pass
+
+
+class Joanna_Grudzinska:
+	pass
+
+
+class Olga(Paul_I, Maria_Feodorovna):
+	pass
+
+
+class Alexandra(Paul_I, Maria_Feodorovna):
+	pass
+
+
+class Joseph:
+	pass
+
+
+class Elena(Paul_I, Maria_Feodorovna):
+	pass
+
+
+class Frederick_Louis:
+	pass
+
+# tired
