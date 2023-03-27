62b6922e88d8ea6b47c82c222e242361fcc2eae5
nocarend <58514394+nocarend@users.noreply.github.com>
2023-03-27 08:03:32 +0300

    add task

--- problems-2018-1/n-valikov/problem3.py
+++ problems-2018-1/n-valikov/problem3.py
@@ -6,7 +6,7 @@ import bitarray
 
 def sieve_of_Eratosthenes_list(N: int):
 	numbers_list = [number for number in range(N)]
-	for index in range(2, int(N ** 0.5) + 1):
+	for index in range(2, math.ceil(math.sqrt(N)) + 1):
 		if numbers_list[index] > 0:
 			numbers_list[index * 2:N:index] = [-1] * (
 				math.ceil((N - index * 2) / 1.0 / index))
@@ -15,7 +15,7 @@ def sieve_of_Eratosthenes_list(N: int):
 
 def sieve_of_Eratosthenes_set(N: int):
 	numbers_set = {number for number in range(2, N)}
-	for number in range(2, int(N ** 0.5) + 1):
+	for number in range(2, math.ceil(math.sqrt(N)) + 1):
 		if number in numbers_set:
 			for deletion_number in range(number * 2, N, number):
 				if deletion_number in numbers_set:
@@ -26,10 +26,10 @@ def sieve_of_Eratosthenes_set(N: int):
 def sieve_of_Eratosthenes_bitarray(N: int):
 	numbers_bitarray = bitarray.bitarray('1' * N)
 	numbers_bitarray[0:2] = 0
-	for index in range(2, int(N ** 0.5) + 1):
+	for index in range(2, math.ceil(math.sqrt(N)) + 1):
 		if numbers_bitarray[index]:
 			numbers_bitarray[index * 2:N:index] = 0
-	return [number for number in range(2, N) if numbers_bitarray[number]]
+	return numbers_bitarray.search(1)
 
 
 if __name__ == '__main__':
@@ -37,10 +37,10 @@ if __name__ == '__main__':
 	N += 1
 	start_time: float = time.time()
 	sieve_of_Eratosthenes_list(N)
-	print(f'List: {time.time() - start_time}s')
+	print(f'List: {(time.time() - start_time):.5f}s')
 	start_time: float = time.time()
 	sieve_of_Eratosthenes_set(N)
-	print(f'Set: {time.time() - start_time}s')
+	print(f'Set: {(time.time() - start_time):.5f}s')
 	start_time: float = time.time()
 	sieve_of_Eratosthenes_bitarray(N)
-	print(f'Bitarray: {time.time() - start_time}s')
+	print(f'Bitarray: {(time.time() - start_time):.5f}s')
