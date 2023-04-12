ab07e933340cdd428292a1c27ffd30b561fda35b
nocarend <gudvin0203@gmail.com>
2023-04-12 14:23:39 +0300

    fix

--- problems-1/n-valikov/problem4.py
+++ problems-1/n-valikov/problem4.py
@@ -1,49 +1,25 @@
-class Solution():
-	integer_to_lexems = [',', '.', 'Ten', 'green bottle', 's',
-	                     'hanging on the wall', 'And', 'if', 'one',
-	                     'should accidentally fall',
-	                     'There`ll be', 'nine', 'Nine', 'eight', 'One',
-	                     'If', 'that', 'no']
+def common_print_first(to_print: str) -> None:
+	print(
+		2 * f"{to_print.capitalize()} green bottles hanging on the wall,"
+		    f"\n",
+		end='')
 
-	@staticmethod
-	def print_third_seven_lines() -> None:
-		integer_to_lexems = Solution.integer_to_lexems
-		print(
-			f"{integer_to_lexems[6]} {integer_to_lexems[7]}"
-			f" {integer_to_lexems[8]} "
-			f"{integer_to_lexems[3]} {integer_to_lexems[9]}"
-			f"{integer_to_lexems[0]}")
 
-	@staticmethod
-	def solve() -> None:
-		integer_to_lexems = Solution.integer_to_lexems
-		print(2 * f"{integer_to_lexems[2]} {integer_to_lexems[3]}"
-		          f"{integer_to_lexems[4]} "
-		          f"{integer_to_lexems[5]}{integer_to_lexems[0]}\n", end='')
-		Solution.print_third_seven_lines()
-		print(
-			f"{integer_to_lexems[10]} {integer_to_lexems[11]} "
-			f"{integer_to_lexems[3]} "
-			f"{integer_to_lexems[4]}{integer_to_lexems[5]}"
-			f"{integer_to_lexems[1]}")
-		print(
-			2 * f"{integer_to_lexems[12]} {integer_to_lexems[3]}"
-			    f"{integer_to_lexems[4]} "
-			    f"{integer_to_lexems[5]}{integer_to_lexems[0]}\n", end='')
-		Solution.print_third_seven_lines()
-		print(f"{integer_to_lexems[10]} {integer_to_lexems[13]} "
-		      f"{integer_to_lexems[3]}{integer_to_lexems[4]} "
-		      f"{integer_to_lexems[5]}{integer_to_lexems[1]}")
-		print(3 * f"{integer_to_lexems[1]}")
-		print(2 * f"{integer_to_lexems[14]} {integer_to_lexems[3]} "
-		          f"{integer_to_lexems[5]}{integer_to_lexems[0]}\n", end='')
-		print(f"{integer_to_lexems[-3]} {integer_to_lexems[-2]} "
-		      f"{integer_to_lexems[8]} {integer_to_lexems[3]} "
-		      f"{integer_to_lexems[9]}")
-		print(f"{integer_to_lexems[10]} {integer_to_lexems[-1]} "
-		      f"{integer_to_lexems[3]}{integer_to_lexems[4]} "
-		      f"{integer_to_lexems[5]}{integer_to_lexems[1]}")
+def common_print_second(to_print: str) -> None:
+	print(f"There’ll be {to_print} green bottles hanging on the wall.")
+
+
+def solve() -> None:
+	words = ['ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three',
+	         'two', 'one', 'no']
+	for i in range(len(words) - 2):
+		common_print_first(words[i])
+		print("And if one green bottle should accidentally fall,")
+		common_print_second(words[i + 1])
+	common_print_first(words[-2])
+	print("If that one green bottle should accidentally fall")
+	common_print_second(words[-1])
 
 
 if __name__ == '__main__':
-	Solution.solve()
+	solve()
