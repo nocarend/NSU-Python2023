9cf74fea1d2827db994f67113b2194ac4f15469c
nocarend <gudvin0203@gmail.com>
2023-04-18 10:51:03 +0300

    do generator solution

--- problems-2/n-valikov/problem1.py
+++ problems-2/n-valikov/problem1.py
@@ -1,7 +1,7 @@
 def solve(n: int) -> [(int, int, int)]:
 	n += 1
-	return [(x, y, z) for x in range(1, n) for y in range(x, n) for z in
-	        range(y, n) if x ** 2 + y ** 2 == z ** 2]
+	return list(((x, y, z) for x in range(1, n) for y in range(x, n) for z in
+	        range(y, n) if x ** 2 + y ** 2 == z ** 2))
 
 
 if __name__ == '__main__':
