65012ebe30ce2f9629f5d94151280c57a77df104
nocarend <gudvin0203@gmail.com>
2023-04-18 11:04:15 +0300

    add exception at wrong filename

--- problems-2/n-valikov/problem2.py
+++ problems-2/n-valikov/problem2.py
@@ -1,11 +1,14 @@
 def solve(filename: str) -> None:
 	result = dict()
-	with open(filename, 'r') as file:
-		for line in file:
-			lexems = line.replace(',', '').split()
-			for word in lexems[2:]:
-				result[word] = result[word] + [
-					lexems[0]] if word in result.keys() else [lexems[0]]
+	try:
+		with open(filename, 'r') as file:
+			for line in file:
+				lexems = line.replace(',', '').split()
+				for word in lexems[2:]:
+					result[word] = result[word] + [
+						lexems[0]] if word in result.keys() else [lexems[0]]
+	except FileNotFoundError as e:
+		print("File not found!@#$%")
 	result = dict(sorted(result.items()))
 	for key, values in result.items():
 		print(key, '-', ', '.join(value for value in values))
