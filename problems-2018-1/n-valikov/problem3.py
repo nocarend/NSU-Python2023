dc3a614ae053cd29100caced531be9712e461273
nocarend <gudvin0203@gmail.com>
2023-04-25 09:44:38 +0300

    syntax

--- problems-2018-1/n-valikov/problem3.py
+++ problems-2018-1/n-valikov/problem3.py
@@ -5,12 +5,12 @@ import bitarray
 
 
 def sieve_of_Eratosthenes_list(N: int):
-	numbers_list = [number for number in range(N)]
+	numbers_list = list(range(N))
 	for index in range(2, math.ceil(math.sqrt(N)) + 1):
 		if numbers_list[index] > 0:
-			numbers_list[index * 2:N:index] = [-1] * (
-				math.ceil((N - index * 2) / 1.0 / index))
-	return [number for number in numbers_list[2:] if number > 0]
+			for eratosthenesIndex in range(index * 2, N, index):
+				numbers_list[eratosthenesIndex] = 0
+	return list(filter(lambda x: x > 1, numbers_list))
 
 
 def sieve_of_Eratosthenes_set(N: int):
@@ -39,8 +39,8 @@ if __name__ == '__main__':
 	sieve_of_Eratosthenes_list(N)
 	print(f'List: {(time.time() - start_time):.5f}s')
 	start_time: float = time.time()
-	sieve_of_Eratosthenes_set(N)
-	print(f'Set: {(time.time() - start_time):.5f}s')
-	start_time: float = time.time()
 	sieve_of_Eratosthenes_bitarray(N)
 	print(f'Bitarray: {(time.time() - start_time):.5f}s')
+	start_time: float = time.time()
+	sieve_of_Eratosthenes_set(N)
+	print(f'Set: {(time.time() - start_time):.5f}s')
