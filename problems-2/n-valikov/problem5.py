a69b2d9a44be683ef5e10d128501f61078a32cd3
nocarend <gudvin0203@gmail.com>
2023-05-29 05:46:33 +0300

    add handling int(input())

--- problems-2/n-valikov/problem5.py
+++ problems-2/n-valikov/problem5.py
@@ -1,4 +1,4 @@
-def solve(n: int) -> [int]:
+﻿def solve(n: int) -> [int]:
 	result = [2] if n > 1 else []
 	result.extend(
 		[number for number in range(3, n + 1, 2) if number % 2 and all(
@@ -9,4 +9,11 @@ def solve(n: int) -> [int]:
 
 if __name__ == '__main__':
 	print("Please, print a number.")
-	print(solve(int(input())))
+	try:
+		number = int(input())
+	except ValueError:
+		from sys import stderr
+
+		stderr.write("Ожидалось число. Например: 7")
+		exit(1)
+	print(solve(number))
